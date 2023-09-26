6b2e27c6006492e117e5e955ab0642fce6f6e198
Strange-Quark-007 <masteryasin@ymail.com>
2023-09-26 13:28:23 +0530

    Feed Done

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,48 @@
-import React from 'react';
+"use client";
+import { useState, useEffect } from "react";
+import PromptCard from '@components/PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+    return (
+        <div className="mt-16 prompt_layout">
+            {
+                data.map((post) => <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />)
+            }
+        </div>
+    );
+};
+
+
 
 const Feed = () => {
+
+    const [searchText, setSearchText] = useState('');
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch('/api/prompt');
+            const data = await response.json();
+            setPosts(data);
+        };
+        fetchPosts();
+    }, []);
+
+
+    const handleSearchChange = (e) => {
+        setSearchText(e.target.value);
+    };
+
+
     return (
-        <div>Feed</div>
+        <section className="feed">
+            <form className="relative w-full flex-center">
+                <input type="text" placeholder="search here..." className="search_input peer"
+                    value={searchText} onChange={handleSearchChange} required></input>
+            </form>
+
+            <PromptCardList data={posts} handleTagClick={() => { }} />
+        </section>
     );
 };
 

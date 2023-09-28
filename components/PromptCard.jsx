8197a339a57f17b879365dbe23fb995d9e6740d5
Strange-Quark-007 <masteryasin@ymail.com>
2023-09-28 14:36:29 +0530

    Profile Complete

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -3,11 +3,15 @@
 import { useState } from "react";
 import Image from "next/image";
 import { useSession } from 'next-auth/react';
-import { usePathName, useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
 
     const [copied, setCopied] = useState('');
+
+    const { data: session } = useSession();
+    const pathName = usePathname();
+    const router = useRouter();
 
     const handleCopy = () => {
         setCopied(post.prompt);
@@ -49,6 +53,12 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             <p className="font-inter text-sm blue_gradient cursor-pointer" onClick={() => handleTagClick && handleTagClick(post.tag)}>
                 {post.tag}
             </p>
+            {session?.user.id === post.creator._id && pathName === '/profile' && (
+                <div className="mt-5 flex gap-4 border-t border-gray-100 pt-3">
+                    <p className="font-inter text-sm green_gradient cursor-pointer" onClick={handleEdit}>Edit</p>
+                    <p className="font-inter text-sm orange_gradient cursor-pointer" onClick={handleDelete}>Delete</p>
+                </div>
+            )}
         </div>
     );
 };

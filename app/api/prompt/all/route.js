3416ce25ddbf2085feb3544186dbe5305cb2e06e
Strange-Quark-007 <masteryasin@ymail.com>
2023-09-29 22:37:07 +0530

    vercel cache fix

--- app/api/prompt/all/route.js
+++ app/api/prompt/all/route.js
@@ -6,7 +6,13 @@ export const GET = async (req) => {
     try {
         await connectToDB();
         const prompts = await Prompt.find().populate('creator');
-        return new Response(JSON.stringify(prompts), { status: 200 });
+        return new Response(JSON.stringify(prompts), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json',
+                'Cache-Control': 'no-store',
+            }
+        });
     } catch (error) {
         return new Response("Failed to fetch prompts", { status: 404 });
     }
